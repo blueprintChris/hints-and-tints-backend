5d85fc598426dd875dcd9115e85e27a277a13229
Chris Moran <cmoran1989@hotmail.co.uk>
2024-02-07 00:02:23 +0000

    feat: updated docs for roomcontroller

--- src/controller/RoomController.ts
+++ src/controller/RoomController.ts
@@ -9,40 +9,77 @@ export class RoomController {
     this.roomStore = new RoomStore();
   }
 
+  /**
+   * Gets all rooms
+   * @returns all rooms
+   */
   public getRooms() {
     return this.roomStore;
   }
 
+  /**
+   * Gets a single room by id
+   * @param id room id
+   * @returns Room
+   */
   public getRoomById(id: string): Room {
     return this.roomStore.get(id);
   }
 
+  /**
+   * Checks a rooms player count
+   * @param id room id
+   * @returns true if player count is zero, false otherwise
+   */
   public isRoomEmpty(id: string): boolean {
     return this.getRoomById(id).getPlayers().length === 0 ? true : false;
   }
 
+  /**
+   * Creates an new empty room at specified object key
+   * @param id room id
+   */
   public createRoom(id: string): void {
     const room = new Room(id);
 
     this.roomStore.store(id, room);
   }
 
+  /**
+   * Deletes the room key, remvoing it from the store object
+   * @param id room id
+   */
   public deleteRoom(id: string): void {
     this.roomStore.delete(id);
   }
 
-  public joinRoom(roomId: string, player: Player): void {
+  /**
+   * Joins a room in session and returns an updated player list
+   */
+  public joinRoom(roomId: string, player: Player): Player[] {
     const room = this.getRoomById(roomId);
 
     room.addPlayer(player);
+
+    return room.getPlayers();
   }
 
+  /**
+   * Removes a player from a player list within a single room
+   * @param roomId room id
+   * @param player player to remove
+   */
   public leaveRoom(roomId: string, player: Player): void {
     const room = this.getRoomById(roomId);
 
     room.removePlayer(player);
   }
 
+  /**
+   * Gets the full list of players for a single room
+   * @param roomId room id
+   * @returns Player list
+   */
   public getPlayers(roomId: string): Player[] {
     const room = this.getRoomById(roomId);
 
